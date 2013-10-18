e8bf7abf1a15e0b9b92aa14c71e4676dcc1906eb
Crascer <marco.ferraioli.93@gmail.com>
2013-10-18 18:27:58 +0200

    ok

---
+++ GiocoFesso/Assets/Script/Dispenser.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var coin : Transform[];
+private var currentChild : Transform;
+
+function Start () {
+
+}
+
+function Update () {
+	
+	if(currentChild == null){
+		Random.seed = Time.time;
+		var rnd = Random.Range(0,7);
+		currentChild = Instantiate(coin[rnd], transform.position, transform.rotation);
+		
+	}
+}
